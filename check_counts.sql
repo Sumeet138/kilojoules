75611e4faf4b0b44564202dd086b562c127f9864
Sumeet_Gond <sumitv9009@gmail.com>
2026-04-23 10:46:44 +0530

    chore: remove seed_demo_extra.sql and check_counts.sql from repo

--- check_counts.sql
+++
@@ -1,5 +0,0 @@
-SELECT 'class_bookings' AS tbl, COUNT(*) AS n FROM class_bookings
-UNION ALL SELECT 'workout_history', COUNT(*) FROM workout_history
-UNION ALL SELECT 'diet_plans', COUNT(*) FROM diet_plans
-UNION ALL SELECT 'bmi_records', COUNT(*) FROM bmi_records
-UNION ALL SELECT 'transactions', COUNT(*) FROM transactions;
